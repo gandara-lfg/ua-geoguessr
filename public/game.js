b2ae79ffed25823da31fc3fa9b8983b9240ea292
gandara <emiganguev@gmail.com>
2026-07-19 18:56:02 -0700

    working with the maps and markers

--- public/game.js
+++ public/game.js
@@ -1,5 +1,7 @@
 let panorama
 let map
+let currentGuess = null
+let guessMarker = null
 
 function initMaps() {
 
@@ -45,9 +47,11 @@ function loadMaps(){
     const mapDiv = document.getElementById("map")
     const mapOptions = {
         center: {lat: 32.231859, lng: -110.951440},
-        mapTypeId: "satellite",
-        zoom: 15,
-        minZoom: 14
+        mapTypeId: "roadmap",
+        zoom: 14,
+        minZoom: 12,
+        mapId: "7e3ea53fed9dd6313e302224",
+        disableDefaultUI: true
     }
 
     panorama = new google.maps.StreetViewPanorama(screenDiv, panOptions)
@@ -55,6 +59,20 @@ function loadMaps(){
 
     map.addListener("click", (mapsMouseEvent) => {
         const clickedLocation = mapsMouseEvent.latLng
+        currentGuess = { lat: clickedLocation.lat(), lng: clickedLocation.lng() }
         console.log("clicked at: " + clickedLocation.toString())
+        handleMapClicks(currentGuess)
     })
+
+}
+
+function handleMapClicks(pos){
+    if (guessMarker == null) {
+        guessMarker = new google.maps.marker.AdvancedMarkerElement({
+        map: map,
+        position: pos,
+    })}
+    else {
+        guessMarker.position = pos
+    }
 }
